0b5f713db76d163ca8c7c1420f42fd81be8b7805
jkidd20 <statkidd@live.unc.edu>
2017-10-24 14:09:22 -0400

    Added lines for possible paper and data

--- Work File.R
+++ Work File.R
@@ -3,3 +3,12 @@
 library(BiocInstaller)
 biocLite("curatedBladderData")
 library(curatedBladderData)
+
+browseVignettes("curatedBladderData")
+data(package="curatedBladderData")
+
+# Link to a good reference paper
+# https://www.ncbi.nlm.nih.gov/pubmed/15930339
+
+# Load data
+data(GSE1827_eset)
