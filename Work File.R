2c711d859cb1fec1d71e84c2ac25ec4bbe17b573
jkidd20 <statkidd@live.unc.edu>
2017-11-08 13:18:54 -0500

    Creation of Function and base analysis

--- Work File.R
+++ Work File.R
@@ -41,13 +41,66 @@ a1 = GSE1827_eset
 
 ####### Suggested to use limma (lmFit, etc) to fit model, find prediction
 ### Mike will give us code
+set.seed(2017)
 library(limma)
 
 eSet = exprs(a1)
-tumType = pData(a1)$summarystage
-age = pData(a1)$age
-gender = pData(a1)$gender
+
+invNum = which(pData(a1)$summarystage == "invasive")
+supNum = which(pData(a1)$summarystage == "superficial")
+
+trainSamp = c(sample(invNum, 15), sample(supNum, 15))
+predSamp = seq_len(ncol(a1))[!(seq_len(ncol(a1)) %in% trainSamp)]
+
+toTrain = a1[, trainSamp]
+
+tumType = pData(toTrain)$summarystage
 design <- model.matrix(~ tumType)
-fit <- lmFit(eSet, design)
+train.eSet = exprs(toTrain)
+
+fit <- lmFit(train.eSet, design)
 fit <- eBayes(fit)
 tab <- topTable(fit, n=nrow(eSet), sort.by="none")
+
+
+bigSet.g = rownames(tab)[tab$adj.P.Val < 0.001 & !is.na(tab$adj.P.Val)]
+smallSet.g = rownames(tab)[tab$adj.P.Val < 0.0001 & !is.na(tab$adj.P.Val)]
+
+
+toPred = a1[, predSamp]
+toPred.expr.t = data.frame(exprs(toPred))
+toPred.stage = pData(toPred)$summarystage
+# impute missing values
+library(caret)
+library(e1071)
+imputedV = preProcess(toPred.expr.t, method = "knnImpute")
+toPred.expr.imp = predict(imputedV, toPred.expr.t)
+
+toPred.bigexp = exprs(toPred)[rownames(toPred) %in% bigSet.g, ] 
+toPred.smallexp = exprs(toPred)[rownames(toPred) %in% smallSet.g, ] 
+
+##### Prediction testing
+
+cvError = function(geneSet, label, expSet, nFold = 10, fitMethod = "rf"){
+  toSample = matrix(1:nFold, 1, length(label))
+  cvNum = sample(toSample)
+  numWrong = rep(0, nFold)
+  for(i in 1:nFold){
+      # set up Train set
+      trainDF = data.frame(label[cvNum != i], t(expSet)[cvNum != i, rownames(expSet) %in% geneSet] )
+      colnames(trainDF)[1] = "label"
+       # train data
+      fit = train(label ~ ., data = trainDF, method = fitMethod)
+      
+      # set up Test Set
+      testDF = data.frame(label[cvNum == i], t(expSet)[cvNum == i, rownames(expSet) %in% geneSet] )
+      colnames(testDF)[1] = "label"
+      
+      predValues = predict(fit, testDF)
+      numWrong[i] = sum(as.character(predValues) != as.character(testDF$label))
+  }
+  mean(numWrong / table(cvNum))
+}
+
+bigError = cvError(bigSet.g, toPred.stage, toPred.expr.imp)
+smallError = cvError(smallSet.g, toPred.stage, toPred.expr.imp)
