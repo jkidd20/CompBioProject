ab05c654efb29cb4fb1129fe1370f044b58fc887
jkidd20 <statkidd@live.unc.edu>
2017-11-20 11:53:29 -0500

    Lots of graphs

--- John Work.R
+++ John Work.R
@@ -14,144 +14,117 @@ set.seed(2017)
 tTime = Sys.time()
 ss.p1.rf = geneCV(a1, pData(a1)$summarystage)
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.p1.ada = geneCV(a1, pData(a1)$summarystage, fitMethod = "ada")
-# Sys.time() - tTime
 tTime = Sys.time()
 ss.p1.glmb = geneCV(a1, pData(a1)$summarystage, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.p1.adab = geneCV(a1, pData(a1)$summarystage, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 tTime = Sys.time()
 ss.p2.rf = geneCV(a1, pData(a1)$summarystage, pCut = 0.00001)
 Sys.time() - tTime
-#tTime = Sys.time()
-#ss.p2.ada = geneCV(a1, pData(a1)$summarystage, pCut = 0.00001, fitMethod = "ada")
-#Sys.time() - tTime
 tTime = Sys.time()
 ss.p2.glmb = geneCV(a1, pData(a1)$summarystage, pCut = 0.00001, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.p2.adab = geneCV(a1, pData(a1)$summarystage, pCut = 0.00001, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 tTime = Sys.time()
 ss.p3.rf = geneCV(a1, pData(a1)$summarystage, pCut = 0.0001)
 Sys.time() - tTime
-#tTime = Sys.time()
-#ss.p3.ada = geneCV(a1, pData(a1)$summarystage, pCut = 0.0001, fitMethod = "ada")
-#Sys.time() - tTime
 tTime = Sys.time()
 ss.p3.glmb = geneCV(a1, pData(a1)$summarystage, pCut = 0.0001, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.p3.adab = geneCV(a1, pData(a1)$summarystage, pCut = 0.0001, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 tTime = Sys.time()
 ss.p4.rf = geneCV(a1, pData(a1)$summarystage, pCut = 0.001)
 Sys.time() - tTime
-#tTime = Sys.time()
-#ss.p4.ada = geneCV(a1, pData(a1)$summarystage, pCut = 0.001, fitMethod = "ada")
-#Sys.time() - tTime
 tTime = Sys.time()
 ss.p4.glmb = geneCV(a1, pData(a1)$summarystage, pCut = 0.001, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.p4.adab = geneCV(a1, pData(a1)$summarystage, pCut = 0.001, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 ### Most Significant
 tTime = Sys.time() 
 ss.ns1.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 50)
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.ns1.ada = geneCV(a1, pData(a1)$summarystage, nSelect = 50, fitMethod = "ada")
-# Sys.time() - tTime
 tTime = Sys.time()
 ss.ns1.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 50, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.ns1.adab = geneCV(a1, pData(a1)$summarystage, nSelect = 50, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 tTime = Sys.time()
 ss.ns2.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 100)
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.ns2.ada = geneCV(a1, pData(a1)$summarystage, nSelect = 100, fitMethod = "ada")
-# Sys.time() - tTime
 tTime = Sys.time()
 ss.ns2.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 100, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.ns2.adab = geneCV(a1, pData(a1)$summarystage, nSelect = 100, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 tTime = Sys.time()
 ss.ns3.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 200)
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.ns3.ada = geneCV(a1, pData(a1)$summarystage, nSelect = 200, fitMethod = "ada")
-# Sys.time() - tTime
 tTime = Sys.time()
 ss.ns3.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 200, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.ns3.adab = geneCV(a1, pData(a1)$summarystage, nSelect = 200, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 # Randomly Selected
 tTime = Sys.time()
 ss.rs1.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 50, randomFlag = TRUE)
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.rs1.ada = geneCV(a1, pData(a1)$summarystage, nSelect = 50, 
-#                     randomFlag = TRUE, fitMethod = "ada")
-# Sys.time() - tTime
 tTime = Sys.time()
 ss.rs1.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 50,
                      randomFlag = TRUE, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.rs1.adab = geneCV(a1, pData(a1)$summarystage, nSelect = 50,
-#                      randomFlag = TRUE, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 tTime = Sys.time()
 ss.rs2.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 100)
 Sys.time() - tTime
-#tTime = Sys.time()
-#ss.rs2.ada = geneCV(a1, pData(a1)$summarystage, nSelect = 100,
-#                    randomFlag = TRUE, fitMethod = "ada")
-#Sys.time() - tTime
 tTime = Sys.time()
 ss.rs2.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 100,
                      randomFlag = TRUE, fitMethod = "glmboost")
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.rs2.adab = geneCV(a1, pData(a1)$summarystage, nSelect = 100,
-#                      randomFlag = TRUE, fitMethod = "adaboost")
-# Sys.time() - tTime
 
 tTime = Sys.time()
 ss.rs3.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 200, randomFlag = TRUE)
 Sys.time() - tTime
-# tTime = Sys.time()
-# ss.rs3.ada = geneCV(a1, pData(a1)$summarystage, nSelect = 200,
-#                     randomFlag = TRUE, fitMethod = "ada")
-# Sys.time() - tTime
 tTime = Sys.time()
 ss.rs3.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 200,
                      randomFlag = TRUE, fitMethod = "glmboost")
 Sys.time() - tTime
-#tTime = Sys.time()
-#ss.rs3.adab = geneCV(a1, pData(a1)$summarystage, nSelect = 200,
-#                     randomFlag = TRUE, fitMethod = "adaboost")
-#Sys.time() - tTime
 
 fullTime = wholeTime = proc.time()
 
 #### heatmaps
+myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occur Once", occur = 1)
+myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs Twice", occur = 2)
+myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs in All", occur = 5)
+
+myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occur Once", occur = 1)
+myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occurs Twice", occur = 2)
+myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occurs in All", occur = 5)
+
+myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occur Once", occur = 1)
+myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occurs Twice", occur = 2)
+myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occurs in All", occur = 5)
+
+myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occur Once", occur = 1)
+myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occurs Twice", occur = 2)
+myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occurs in All", occur = 5)
+
+myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occur Once", occur = 1)
+myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occurs Twice", occur = 2)
+myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occurs in All", occur = 5)
+
+myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occur Once", occur = 1)
+myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occurs Twice", occur = 2)
+myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occurs in All", occur = 5)
+
+myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occur Once", occur = 1)
+myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occurs Twice", occur = 2)
+myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occurs in All", occur = 5)
+
+myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occur Once", occur = 1)
+myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occurs Twice", occur = 2)
+myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occurs in All", occur = 5)
+
+myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occur Once", occur = 1)
+myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occurs Twice", occur = 2)
+myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occurs in All", occur = 5)
+
+myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occur Once", occur = 1)
+myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occurs Twice", occur = 2)
+myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occurs in All", occur = 5)
