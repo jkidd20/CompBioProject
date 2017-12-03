ffea7af732c96471bea111a1c743ed7ff42866a8
jkidd20 <statkidd@live.unc.edu>
2017-12-03 00:03:01 -0500

    Updated function file for multiple cross validation runs

--- Function File.R
+++ Function File.R
@@ -6,7 +6,7 @@ library(pheatmap)
 library(RColorBrewer)
 
 geneCV = function(expData, label, pCut = .000001, nFold = 5, fitMethod = "rf", impMethod = "medianImpute", 
-                  randomFlag = FALSE, nSelect = NA){
+                  randomFlag = FALSE, nSelect = NA, nrep = 10){
     eSet.t = exprs(expData)
     label = as.factor(label)
     labLev = levels(label)
@@ -17,64 +17,80 @@ geneCV = function(expData, label, pCut = .000001, nFold = 5, fitMethod = "rf", i
     imputedV = preProcess(t(eSet.t), method = impMethod)
     eSet = t(predict(imputedV, t(eSet.t)))
     
-    ### Cross Validation
-    toSample1 = matrix(1:nFold, 1, length(g1Num))
-    cvNum1 = sample(toSample1)
-    toSample2 = matrix(1:nFold, 1, length(g2Num))
-    cvNum2 = sample(toSample2)
-    
-    numWrong = rep(0, nFold)
-    genes = list()
-    
-    for(i in 1:nFold){
-      # set up Train and Test set
-      trainLabels = c(rep(labLev[1], sum(cvNum1 != i)), rep(labLev[2], sum(cvNum2 != i)))
-      testLabels = c(rep(labLev[1], sum(cvNum1 == i)), rep(labLev[2], sum(cvNum2 == i)))
+    retValue = list()
+    for(repNum in seq_len(nrep)){
+      ### Cross Validation
+      toSample1 = matrix(1:nFold, 1, length(g1Num))
+      cvNum1 = sample(toSample1)
+      toSample2 = matrix(1:nFold, 1, length(g2Num))
+      cvNum2 = sample(toSample2)
       
-      trainExprs = rbind(t(eSet)[g1Num[cvNum1 != i], ], t(eSet)[g2Num[cvNum2 != i], ])
-      testExprs = rbind(t(eSet)[g1Num[cvNum1 == i], ], t(eSet)[g2Num[cvNum2 == i], ])
+      numWrong = rep(0, nFold)
+      genes = list()
       
-      
-      trainDF = data.frame(trainLabels, trainExprs)
-      testDF = data.frame(testLabels, testExprs)
-      
-      colnames(trainDF)[1] = colnames(testDF)[1] = "label"
-      
-      # train data
-      if(!randomFlag){
-        design <- model.matrix(~ trainDF[, 1])
-        fit <- lmFit(t(trainExprs), design)
-        fit <- eBayes(fit)
-        tab <- topTable(fit, n=nrow(eSet), sort.by="none")
-        if(is.na(nSelect)){
-          genes[[i]] = gsub('-', '.', rownames(tab)[tab$adj.P.Val < pCut])
+      for(i in 1:nFold){
+        # set up Train and Test set
+        trainLabels = c(rep(labLev[1], sum(cvNum1 != i)), rep(labLev[2], sum(cvNum2 != i)))
+        testLabels = c(rep(labLev[1], sum(cvNum1 == i)), rep(labLev[2], sum(cvNum2 == i)))
+        
+        trainExprs = rbind(t(eSet)[g1Num[cvNum1 != i], ], t(eSet)[g2Num[cvNum2 != i], ])
+        testExprs = rbind(t(eSet)[g1Num[cvNum1 == i], ], t(eSet)[g2Num[cvNum2 == i], ])
+        
+        
+        trainDF = data.frame(trainLabels, trainExprs)
+        testDF = data.frame(testLabels, testExprs)
+        
+        colnames(trainDF)[1] = colnames(testDF)[1] = "label"
+        
+        # train data
+        if(!randomFlag){
+          design <- model.matrix(~ trainDF[, 1])
+          fit <- lmFit(t(trainExprs), design)
+          fit <- eBayes(fit)
+          tab <- topTable(fit, n=nrow(eSet), sort.by="none")
+          if(is.na(nSelect)){
+            genes[[i]] = gsub('-', '.', rownames(tab)[tab$adj.P.Val < pCut])
+          }
+          else{
+            genes[[i]] = gsub('-', '.', rownames(tab[order(tab$adj.P.Val),])[1:nSelect])
+          }
         }
         else{
-          genes[[i]] = gsub('-', '.', rownames(tab[order(tab$adj.P.Val),])[1:nSelect])
+          genes[[i]] = gsub('-', '.', sample(rownames(expData), nSelect))
         }
+        
+        smallTrain = cbind(trainDF[, 1], trainDF[, colnames(trainDF) %in% genes[[i]]])
+        colnames(smallTrain)[1] = "label"
+        
+        mFit = train(label ~ ., data = smallTrain, method = fitMethod)
+        
+        # Predict on Test
+        
+        predValues = predict(mFit, testDF)
+        numWrong[i] = sum(as.character(predValues) != as.character(testDF$label))
       }
-      else{
-        genes[[i]] = gsub('-', '.', sample(rownames(expData), nSelect))
-      }
-      
-      smallTrain = cbind(trainDF[, 1], trainDF[, colnames(trainDF) %in% genes[[i]]])
-      colnames(smallTrain)[1] = "label"
-      
-      mFit = train(label ~ ., data = smallTrain, method = fitMethod)
-      
-      # Predict on Test
-      
-      predValues = predict(mFit, testDF)
-      numWrong[i] = sum(as.character(predValues) != as.character(testDF$label))
+      retValue[[repNum]] = list(genes = genes, cvError = mean(numWrong / table(c(cvNum1, cvNum2))), 
+                       impMethod = impMethod, label = label)
     }
-    return(list(genes = genes, cvError = mean(numWrong / table(c(cvNum1, cvNum2))), 
-                impMethod = impMethod, label = label))
+    return(retValue)
 }
 
+aveCVerror = function(geneCVres){
+  aveCVerror = 0
+  for(i in seq_len(length(geneCVres))){
+    aveCVerror = aveCVerror + geneCVres[[i]]$cvError
+  }
+  return(aveCVerror / length(geneCVres))
+}
 
 #### Function to create heatmaps from first function
 myMap = function(cvRes, expData, titleIn = "", occur = 1){
-  plotGenes = names(table(unlist(cvRes$genes)))[table(unlist(cvRes$genes)) >= occur]
+  plotGenes = NULL
+  for(i in seq_len(length(cvRes))){
+    plotGenes = c(plotGenes, 
+              names(table(unlist(cvRes[[i]]$genes)))[table(unlist(cvRes[[i]]$genes)) >= occur])
+  }
+  plotGenes = names(table(plotGenes))
 
   eSet.t = exprs(expData)
   label = cvRes$label
--- John Work.R
+++ John Work.R
@@ -54,12 +54,14 @@ tTime = Sys.time()
 ss.ns2.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 100, fitMethod = "glmboost")
 Sys.time() - tTime
 
+
 tTime = Sys.time()
 ss.ns3.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 200)
 Sys.time() - tTime
 tTime = Sys.time()
 ss.ns3.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 200, fitMethod = "glmboost")
 Sys.time() - tTime
+
 
 # Randomly Selected
 tTime = Sys.time()
@@ -70,13 +72,15 @@ ss.rs1.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 50,
                      randomFlag = TRUE, fitMethod = "glmboost")
 Sys.time() - tTime
 
+
 tTime = Sys.time()
-ss.rs2.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 100)
+ss.rs2.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 100, randomFlag = TRUE)
 Sys.time() - tTime
 tTime = Sys.time()
 ss.rs2.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 100,
                      randomFlag = TRUE, fitMethod = "glmboost")
 Sys.time() - tTime
+
 
 tTime = Sys.time()
 ss.rs3.rf = geneCV(a1, pData(a1)$summarystage, nSelect = 200, randomFlag = TRUE)
@@ -86,45 +90,105 @@ ss.rs3.glmb = geneCV(a1, pData(a1)$summarystage, nSelect = 200,
                      randomFlag = TRUE, fitMethod = "glmboost")
 Sys.time() - tTime
 
-fullTime = wholeTime = proc.time()
+ss.p1.svm = geneCV(a1, pData(a1)$summarystage, fitMethod = "svmLinearWeights")
+ss.p2.svm = geneCV(a1, pData(a1)$summarystage, pCut = 0.00001, fitMethod = "svmLinearWeights")
+ss.p3.svm = geneCV(a1, pData(a1)$summarystage, pCut = 0.0001, fitMethod = "svmLinearWeights")
+ss.p4.svm = geneCV(a1, pData(a1)$summarystage, pCut = 0.001, fitMethod = "svmLinearWeights")
+ss.ns1.svm = geneCV(a1, pData(a1)$summarystage, nSelect = 50, fitMethod = "svmLinearWeights")
+ss.ns2.svm = geneCV(a1, pData(a1)$summarystage, nSelect = 100, fitMethod = "svmLinearWeights")
+ss.ns3.svm = geneCV(a1, pData(a1)$summarystage, nSelect = 200, fitMethod = "svmLinearWeights")
+ss.rs1.svm = geneCV(a1, pData(a1)$summarystage, nSelect = 50, 
+                    randomFlag = TRUE, fitMethod = "svmLinearWeights")
+ss.rs2.svm = geneCV(a1, pData(a1)$summarystage, nSelect = 100, 
+                    randomFlag = TRUE, fitMethod = "svmLinearWeights")
+ss.rs3.svm = geneCV(a1, pData(a1)$summarystage, nSelect = 200, 
+                    randomFlag = TRUE, fitMethod = "svmLinearWeights")
 
-#### heatmaps
-myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occur Once", occur = 1)
-myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs Twice", occur = 2)
-myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs in All", occur = 5)
+fullTime = proc.time() - wholeTime
 
-myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occur Once", occur = 1)
-myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occurs Twice", occur = 2)
-myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occurs in All", occur = 5)
+# #### heatmaps
+# myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occur Once", occur = 1)
+# myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs Twice", occur = 2)
+# myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs in All", occur = 5)
+# 
+# myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occur Once", occur = 1)
+# myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occurs Twice", occur = 2)
+# myMap(ss.p2.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.0001) - Occurs in All", occur = 5)
+# 
+# myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occur Once", occur = 1)
+# myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occurs Twice", occur = 2)
+# myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occurs in All", occur = 5)
+# 
+# myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occur Once", occur = 1)
+# myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occurs Twice", occur = 2)
+# myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occurs in All", occur = 5)
+# 
+# myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occur Once", occur = 1)
+# myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occurs Twice", occur = 2)
+# myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occurs in All", occur = 5)
+# 
+# myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occur Once", occur = 1)
+# myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occurs Twice", occur = 2)
+# myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occurs in All", occur = 5)
+# 
+# myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occur Once", occur = 1)
+# myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occurs Twice", occur = 2)
+# myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occurs in All", occur = 5)
+# 
+# myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occur Once", occur = 1)
+# myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occurs Twice", occur = 2)
+# myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occurs in All", occur = 5)
+# 
+# myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occur Once", occur = 1)
+# myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occurs Twice", occur = 2)
+# myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occurs in All", occur = 5)
+# 
+# myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occur Once", occur = 1)
+# myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occurs Twice", occur = 2)
+# myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occurs in All", occur = 5)
 
-myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occur Once", occur = 1)
-myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occurs Twice", occur = 2)
-myMap(ss.p3.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.001) - Occurs in All", occur = 5)
+# #### Final output
+# myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occur Once", occur = 1)
+# myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs in All", occur = 5)
+# 
+# myMap(ss.p1.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.00001) - Occurs in All", occur = 5)
+# myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occurs in All", occur = 5)
+# 
+# myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occur Once", occur = 1)
+# myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occur Once", occur = 1)
+# 
+# myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occurs Twice", occur = 2)
+# myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occurs Twice", occur = 2)
 
-myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occur Once", occur = 1)
-myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occurs Twice", occur = 2)
-myMap(ss.p4.rf, a1, titleIn = "Differentially Expressed Genes (p < 0.01) - Occurs in All", occur = 5)
+###### CV Errors
+ss.p1.rf$cvError
+ss.p2.rf$cvError
+ss.p3.rf$cvError
+ss.p4.rf$cvError
+ss.ns1.rf$cvError
+ss.ns2.rf$cvError
+ss.ns3.rf$cvError
+ss.rs1.rf$cvError
+ss.rs2.rf$cvError
+ss.rs3.rf$cvError
+ss.p1.glmb$cvError
+ss.p2.glmb$cvError
+ss.p3.glmb$cvError
+ss.p4.glmb$cvError
+ss.ns1.glmb$cvError
+ss.ns2.glmb$cvError
+ss.ns3.glmb$cvError
+ss.rs1.glmb$cvError
+ss.rs2.glmb$cvError
+ss.rs3.glmb$cvError
+ss.p1.svm$cvError
+ss.p2.svm$cvError
+ss.p3.svm$cvError
+ss.p4.svm$cvError
+ss.ns1.svm$cvError
+ss.ns2.svm$cvError
+ss.ns3.svm$cvError
+ss.rs1.svm$cvError
+ss.rs2.svm$cvError
+ss.rs3.svm$cvError
 
-myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occur Once", occur = 1)
-myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occurs Twice", occur = 2)
-myMap(ss.ns1.rf, a1, titleIn = "Most Significant Genes - Top 50 - Occurs in All", occur = 5)
-
-myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occur Once", occur = 1)
-myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occurs Twice", occur = 2)
-myMap(ss.ns2.rf, a1, titleIn = "Most Significant Genes - Top 100 - Occurs in All", occur = 5)
-
-myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occur Once", occur = 1)
-myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occurs Twice", occur = 2)
-myMap(ss.ns3.rf, a1, titleIn = "Most Significant Genes - Top 200 - Occurs in All", occur = 5)
-
-myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occur Once", occur = 1)
-myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occurs Twice", occur = 2)
-myMap(ss.rs1.rf, a1, titleIn = "Randomly Selected Genes - 50 - Occurs in All", occur = 5)
-
-myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occur Once", occur = 1)
-myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occurs Twice", occur = 2)
-myMap(ss.rs2.rf, a1, titleIn = "Randomly Selected Genes - 100 - Occurs in All", occur = 5)
-
-myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occur Once", occur = 1)
-myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occurs Twice", occur = 2)
-myMap(ss.rs3.rf, a1, titleIn = "Randomly Selected Genes - 200 - Occurs in All", occur = 5)
